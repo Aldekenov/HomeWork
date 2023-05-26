d95f00d2146c954f5cd87dc5d52ada4d8985fd1f
Aldekenov <aidar.aldekenov@gmail.com>
2023-05-26 21:06:53 +0500

    lesson_4.1

--- lesson_4/kaz/toka/static/index/script.js
+++ lesson_4/kaz/toka/static/index/script.js
@@ -3,10 +3,12 @@ $(window).scroll(function() {
   if(target < 100) {
      $('nav').css('background-color', 'rgba(255, 255, 255, 0.5)');
      $('nav').css('height', '100px');
+     $('#button').css('opacity', '0');
   }
   else {
      $('nav').css('background-color', 'rgba(0, 0, 0, 0.5)');
      $('nav').css('height', '80px');
+     $('#button').css('opacity', '1');
   }
 })
 
@@ -16,8 +18,21 @@ $(".navbar a").on("click", function () {
     $("html, body").animate({
         scrollTop: $(href).offset().top
     }, {
-        duration: 370,   // по умолчанию «400»
-        easing: "linear" // по умолчанию «swing»
+        duration: 370,
+        easing: "swing"
+    });
+
+    return false;
+});
+
+$("#button a").on("click", function () {
+    let href = $(this).attr("href");
+
+    $("html, body").animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 370,
+        easing: "swing"
     });
 
     return false;
