ea310bd621c24cb1504971532fec08b921bbc6e0
Aldekenov <aidar.aldekenov@gmail.com>
2023-07-15 14:57:24 +0500

    15.07.23

---
+++ ClassWork/formsite/newform/static/script/form.js
@@ -0,0 +1,11 @@
+function ph() {
+    $('input[name="name"]').attr('placeholder', 'Ахалай');
+    $('input[name="surname"]').attr('placeholder', 'Махалай');
+    $('input[name="birthday"]').attr('placeholder', '2012-12-12');
+    $('input[name="number"]').attr('placeholder', '87776665544');
+    $('input[name="comment"]').attr('placeholder', 'Анау-мынау короче');
+}
+
+document.addEventListener("DOMContentLoaded", function (){
+    ph()
+})
