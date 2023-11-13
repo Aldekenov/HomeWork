4876909ac598cabf1d001fc450203de23865416d
Aldekenov <aidar.aldekenov@gmail.com>
2023-11-14 01:41:50 +0500

    UPD11

--- Exam/website/bboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from bboard.views import *
-from users.views import *
-
-urlpatterns = [
-    path('', index),
-    # path('students/', students, name="students"),
-]
